91f7065111dcb3209b50d41b583fd51b79ad68e2
chrisae9 <chrisae9@gmail.com>
2020-07-21 16:10:12 -0400

    Initialized Bot

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# chis-bot
-A chis bot
+# A Chis Discord Bot
+
+This bot is the worst bot ever. Don't even try to tell me it is good. Don't USE this!!!
+
+
+## Installation
+
+Idk, I'm not going to tell you. You're not supposed to use this anyways. But here's
+a little hint.
+```
+git pull these-nuts
+```
+
+## Usage
+
+None.
+
+![here](https://i.imgur.com/t3Jh8co.jpg) 
+
+## Contributing
+
+Ya boiz Chis and Sam on the mic. No pull reqests here.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ src/bot.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import discord
+from discord.ext import commands
+
+import json
+import os, sys
+import shutil
+import random
+from functools import reduce
+import re
+from enum import Enum
+import argparse
+import asyncio
+
+import logging
+from logging.handlers import RotatingFileHandler
+
+logger = logging.getLogger('root')
+log_handler = RotatingFileHandler('bot.log', maxBytes=1024*1024*5, backupCount=2)
+logging.basicConfig(level=logging.INFO, handlers=[log_handler, logging.StreamHandler()])
+
+class ChisBot(commands.Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(command_prefix='$', *args, **kwargs)
+
+    def run(self, *args, **kwargs):
+        return super().run(*args, **kwargs)
+
+    async def on_ready(self):
+        logging.info(f'Logged in as "{self.user}".')
+
+
+bot = ChisBot()
+
+@bot.command()
+async def hi(ctx):
+    logging.info(f'said hello to {ctx.author.display_name}')
+    await ctx.send('Sup, chad ;)')
+
+if not os.path.exists('token.txt'):
+    print('Token file not found. Place your Discord token ID in a file called `token.txt`.', file=sys.stderr)
+    sys.exit(1)
+
+with open('token.txt', 'r') as token_file:
+    bot.run(token_file.read())
