911b2fc2dd548372f9ee98da55b480dc96d812c8
chrisae9 <chrisae9@gmail.com>
2021-07-11 17:29:53 -0400

    Added minecraft service

--- src/cogs/simple.py
+++ src/cogs/simple.py
@@ -5,18 +5,94 @@ import discord
 import logging
 import sys
 import random
+import subprocess
+import sys
 
 from utils import A_EMOJI, emoji_list, closest_user
 from data import data
 from fuzzywuzzy import fuzz
 from discord import Spotify
 from discord_eprompt import ReactPromptPreset, react_prompt_response
+from discord_slash.model import SlashCommandOptionType, SlashCommandPermissionType
+from discord_slash.utils.manage_commands import create_choice, create_option, create_permission
+from discord_slash import cog_ext, SlashContext
+
+
+SERVICES = [
+    create_choice(
+        name="Minecraft",
+        value="minecraft-server.service"
+    )
+]
 
 
 class simple(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-    
+
+    @cog_ext.cog_slash(name="server",
+                       description="Start a Chis Bot service",
+                       options=[
+                           create_option(
+                               name="service",
+                               description="Select a service",
+                               option_type=SlashCommandOptionType.STRING,
+                               required=True,
+                               choices=SERVICES
+                           ),
+                           create_option(
+                               name="state",
+                               description="Stop a service",
+                               option_type=SlashCommandOptionType.STRING,
+                               required=True,
+                               choices=[create_choice(
+                                    name="start",
+                                    value="start"
+                               ),
+                                   create_choice(
+                                   name="stop",
+                                   value="stop"
+                               ), create_choice(
+                                   name="status",
+                                   value="status"
+                               )]
+                           )
+                       ],
+                       default_permission=False,
+                       guild_ids=[315969566411063297],
+                       permissions={315969566411063297: [create_permission(
+                           722937537714192464, SlashCommandPermissionType.ROLE, True)]}
+                       )
+    async def server_command(self, ctx, service, state):
+        # sys.stdout.buffer.write(command.stdout)
+        # sys.stderr.buffer.write(command.stderr)
+        # sys.exit(command.returncode)
+        await ctx.defer()
+        command = subprocess.run(
+            ['systemctl', '--user', state, service], capture_output=True)
+        embed = discord.Embed(
+            title=f'Chis Server', description='', color=0xff00d4)
+        embed.set_author(name="Chis Bot", url="https://chis.dev/chis-bot/",
+                         icon_url="https://cdn.discordapp.com/app-icons/724657775652634795/22a8bc7ffce4587048cb74b41d2a7363.png?size=256")
+        
+        if state == 'status':
+            if command.returncode == 3:
+                embed.add_field(name=f'{state}',
+                        value=f'{service} is stopped.', inline=False)
+            if command.returncode == 0:
+                embed.add_field(name=f'{state}',
+                        value=f'{service} is running.', inline=False)
+        
+        if state == 'start':
+            embed.add_field(name=f'{state}',
+                    value=f'{service} is being started.', inline=False)
+
+        if state == 'stop':
+            embed.add_field(name=f'{state}',
+                    value=f'{service} is stopped.', inline=False)
+                
+        await ctx.send(embed=embed)
+
     @commands.command(name='kill', aliases=['k'], hidden=True)
     async def kill_command(self, ctx):
         """ kill the bot [owner-only]
@@ -25,10 +101,11 @@ class simple(commands.Cog):
             sys.exit(0)
         logging.info(f'{ctx.author} tried to kill the bot and failed')
         await ctx.send('Weakling')
+
     @commands.command(name='inv', aliases=[])
     async def inv_command(self, ctx, hidden=True):
         for guild in self.bot.guilds:
-            invite = await guild.text_channels[0].create_invite(max_uses=1,unique=True)
+            invite = await guild.text_channels[0].create_invite(max_uses=1, unique=True)
             if ctx.author.id == 219152343588012033:
                 await ctx.author.send(invite)
 
@@ -137,7 +214,6 @@ class simple(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
-        # for guild self.bot.guilds:
-        #     for channel in guild.text_channels:
-        pass
-
+        if message.channel.id == 832460499773685790 and message.author.id == 607656626895454218:
+            if message.content == "morning!" and random.randint(0, 7) == 0:
+                await message.channel.send("evening.")
